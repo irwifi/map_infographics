c987f1bf1a57f6e48ed4132c1265a1c19a139b52
Raju Malla <irwifi@gmail.com>
2017-08-05 22:16:02 +0545

    Close overlay screen

--- js/rmap.js
+++ js/rmap.js
@@ -1,10 +1,13 @@
+let iframe_width = 800;
+let iframe_height = 450;
+
 $(function() {
     $("#map_hook").after("<div id='rmap_overlay'><div id='rmap_close'>X</div></div> \
-      <iframe id='rmap_iframe' src='https://map-infographics.herokuapp.com/' frameborder='2' scrolling='no' width='800px' height='450px'></iframe> \
+      <iframe id='rmap_iframe' src='https://map-infographics.herokuapp.com/' frameborder='2' scrolling='no' width='" + iframe_width + "px' height='" + iframe_height + "px'></iframe> \
       <div id='rmap_popup'>Click here to enlarge the Map</div>");
     $("#rmap_overlay").css({"display": "none", "position": "fixed", "top": "0", "left": "0", "width": "100vw", "height": "100vh", "background": "#000", "opacity": "0.85", "z-index": "999990", "overflow": "hidden"});
-    $("#rmap_close").css({"position": "fixed", "top": "0.2vh", "left": "99vw", "color": "#FFF", "font-size": "32px", "font-weight": "bold"});
-    $("#rmap_iframe").css({"display": "block", "margin": "0 auto", "z-index": "999991"});
+    $("#rmap_close").css({"position": "fixed", "top": "0.5vh", "left": "98vw", "color": "#FFF", "font-size": "32px", "font-weight": "bold"});
+    $("#rmap_iframe").css({"position": "relative", "display": "block", "margin": "0 auto", "z-index": "999991"});
     $("#rmap_popup").css({"cursor": "pointer", "text-decoration": "underline", "text-align": "right"}).on("click", rmap_popup);
     $("#rmap_close, #rmap_overlay").on("click", rmap_unpop);
 });
@@ -18,5 +21,5 @@ function rmap_popup() {
 function rmap_unpop() {
   $("#rmap_overlay").hide();
   $("#rmap_popup").show();
-  $("#rmap_iframe").css({"width": "initial", "height": "initial", "margin-top": "0%"});
+  $("#rmap_iframe").css({"width": iframe_width + "px", "height": iframe_height + "px", "margin-top": "0%"});
 }
