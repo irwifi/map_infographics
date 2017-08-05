0f5976271aa8feec3caa7b111e8564fff4476590
Raju Malla <irwifi@gmail.com>
2017-08-05 22:25:37 +0545

    Finalizing

--- js/rmap.js
+++ js/rmap.js
@@ -6,7 +6,7 @@ $(function() {
       <iframe id='rmap_iframe' src='https://map-infographics.herokuapp.com/' frameborder='2' scrolling='no' width='" + iframe_width + "px' height='" + iframe_height + "px'></iframe> \
       <div id='rmap_popup'>Click here to enlarge the Map</div>");
     $("#rmap_overlay").css({"display": "none", "position": "fixed", "top": "0", "left": "0", "width": "100vw", "height": "100vh", "background": "#000", "opacity": "0.85", "z-index": "999990", "overflow": "hidden"});
-    $("#rmap_close").css({"position": "fixed", "top": "1vh", "left": "94vw", "color": "#FFF", "font-famly": "", "font-size": "40px", "font-weight": "bold"});
+    $("#rmap_close").css({"position": "fixed", "top": "1vh", "left": "96vw", "color": "#FFF", "font-famly": "", "font-size": "60px", "font-weight": "bold", "cursor": "pointer"});
     $("#rmap_iframe").css({"position": "relative", "display": "block", "margin": "0 auto", "z-index": "999991"});
     $("#rmap_popup").css({"cursor": "pointer", "text-decoration": "underline", "text-align": "right"}).on("click", rmap_popup);
     $("#rmap_close, #rmap_overlay").on("click", rmap_unpop);
