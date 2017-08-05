af5815bbb3aecd2ef9b41a542a17c84d604fdbcd
Raju Malla <irwifi@gmail.com>
2017-08-05 19:22:34 +0545

    Embedding into iframe

---
+++ js/rmap.js
@@ -0,0 +1,3 @@
+$(function() {
+    $("#map_hook").after("<iframe id='rmap_iframe' src='https://map-infographics.herokuapp.com/' frameborder='2' scrolling='no' width='50%' height='50%'></iframe>");
+});
