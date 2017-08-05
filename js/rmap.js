bf94d50e337a3b74e3e226364526c8b0f71c4600
Raju Malla <irwifi@gmail.com>
2017-08-05 19:42:20 +0545

    Simplified map

--- js/rmap.js
+++ js/rmap.js
@@ -1,3 +1,3 @@
 $(function() {
-    $("#map_hook").after("<iframe id='rmap_iframe' src='https://map-infographics.herokuapp.com/' frameborder='2' scrolling='no' width='50%' height='50%'></iframe>");
+    $("#map_hook").after("<iframe id='rmap_iframe' src='https://map-infographics.herokuapp.com/' frameborder='2' scrolling='no' width='800px' height='450px'></iframe>");
 });
