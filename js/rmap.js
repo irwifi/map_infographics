2640540d1424b05951ff837ba3f2e1c24eb0ba30
Raju Malla <irwifi@gmail.com>
2017-08-05 21:00:25 +0545

    Map popup

--- js/rmap.js
+++ js/rmap.js
@@ -1,3 +1,14 @@
 $(function() {
-    $("#map_hook").after("<iframe id='rmap_iframe' src='https://map-infographics.herokuapp.com/' frameborder='2' scrolling='no' width='800px' height='450px'></iframe>");
+    $("#map_hook").after("<div id='rmap_overlay'><div id='rmap_close'>Close</div></div> \
+      <iframe id='rmap_iframe' src='https://map-infographics.herokuapp.com/' frameborder='2' scrolling='no' width='800px' height='450px'></iframe> \
+      <div id='rmap_popup' style='cursor: pointer; text-decoration: underline; text-align: right;'>Click here to enlarge the Map</div>");
+    $("#rmap_overlay").css({"display": "none", "position": "fixed", "width": "100vw", "height": "100vh", "z-index": "999990", "overflow": "hidden"});
+    $("#rmap_iframe").css({"margin": "0 auto", "z-index": "999991"});
+    $("#rmap_popup").on("click", rmap_popup);
 });
+
+function rmap_popup() {
+  $("#rmap_overlay").show();
+  $("#rmap_iframe").css({"width": "86vw", "height": "86vh"});
+  $("#rmap_popup").hide();
+}
